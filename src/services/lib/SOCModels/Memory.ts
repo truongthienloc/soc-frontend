9ef632907add4a06cfce00f99e337e8e10b65e68
ngbn111723 <21520366@gm.uit.edu.vn>
2024-07-31 20:47:53 +0700

    31/7

--- src/services/lib/SOCModels/Memory.ts
+++ src/services/lib/SOCModels/Memory.ts
@@ -11,12 +11,12 @@ export default class Memory {
     slaveMemory: Slave
     constructor(active: boolean, LM_point: number, IO_point: number, 
         Imem_point: number, Dmem_point: number, Stack_point: number) {
+        this.LM_point    = 0
+        this.IO_point    = 0
+        this.Imem_point  = 0
+        this.Dmem_point  = 0 
+        this.Stack_point = 0
         
-        this.LM_point    = LM_point
-        this.IO_point    = IO_point
-        this.Imem_point  = Imem_point
-        this.Dmem_point  = Dmem_point 
-        this.Stack_point = Stack_point
         this.Memory = {}
 
         for (let i = 0; i < LM_point; i+=4) 
@@ -33,6 +33,25 @@ export default class Memory {
         
         this.active = active
         this.slaveMemory = new Slave('DataMemory', true)
+    }
+    public setMemory( LM_point: number, IO_point: number, 
+        Imem_point: number, Dmem_point: number, Stack_point: number){
+            this.LM_point    = LM_point
+            this.IO_point    = IO_point
+            this.Imem_point  = Imem_point
+            this.Dmem_point  = Dmem_point 
+            this.Stack_point = Stack_point
+            for (let i = 0; i < LM_point; i+=4) 
+                this.Memory[i.toString(2).padStart(32,'0')] = '0'.padStart(32,'0')
+            for (let i = LM_point; i < IO_point; i+=4) 
+                this.Memory[i.toString(2).padStart(32,'0')] = '0'.padStart(32,'0')
+            this.Memory[IO_point.toString(2).padStart(32,'0')] = '0'.padStart(32,'0')
+            for (let i = IO_point + 4; i < Imem_point; i+=4) 
+                this.Memory[i.toString(2).padStart(32,'0')] = '0'.padStart(32,'0')
+            for (let i = Imem_point; i < Dmem_point; i+=4) 
+                this.Memory[i.toString(2).padStart(32,'0')] = '0'.padStart(32,'0')
+            for (let i = Dmem_point; i < Stack_point; i+=4) 
+                this.Memory[i.toString(2).padStart(32,'0')] = '0'.padStart(32,'0')
     }
     public reset () {
         for (let i = 0; i < this.LM_point; i+=4) 

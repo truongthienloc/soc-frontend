3ca55e09735c8851bbf08bdfff507622d1a90f12
unknown <truongthienloc2@gmail.com>
2024-08-03 20:58:42 +0700

    feature(led-matrix): change

--- src/components/LedMatrix/LedMatrix.tsx
+++ src/components/LedMatrix/LedMatrix.tsx
@@ -5,13 +5,13 @@ type Props = {
   colCount?: number
 }
 
-export default function LedMatrix({ rowCount = 32, colCount = 32 }: Props) {
+export default function LedMatrix({ rowCount = 96, colCount = 96 }: Props) {
   // const array = Array.from(Array(10).keys())
   // console.log('array: ', array);
 
   return (
     <div
-      className="led-matrix mt-4 grid grid-cols-[repeat(var(--col-count),_0.25rem)] justify-center gap-[1px]"
+      className="led-matrix m-auto grid grid-cols-[repeat(var(--col-count),_0.25rem)] justify-center gap-[1px]"
       style={{ '--col-count': colCount } as React.CSSProperties}
     >
       {Array.from(Array(colCount).keys()).map((row) =>

46d9f35b07b80aa08895e2fc0c8a10f64ad00583
ngbn111723 <21520366@gm.uit.edu.vn>
2024-10-17 21:31:04 +0700

    17/10

--- src/services/lib/SOCModels/SoC.ts
+++ src/services/lib/SOCModels/SoC.ts
@@ -304,6 +304,7 @@ export default class Soc {
             this.println('Cycle ', this.cycle.toString()+' : '+ MMU_message)
             // console.log(MMU_message)
             // this.println(MMU_message)
+            console.log(this.MMU.TLB)
             physical_address = nphysical_address
         } 
         

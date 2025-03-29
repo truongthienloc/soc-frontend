62f97c27c8e112502857342f8c032134248f837f
unknown <truongthienloc2@gmail.com>
2025-03-29 11:03:30 +0700

    refactor(view): change view of led matrix and memory

--- src/components/DMATable/DMATable.tsx
+++ src/components/DMATable/DMATable.tsx
@@ -36,8 +36,6 @@ type Props = {
 
 export default function DMATable({ data, configs }: Props) {
   const [input, setInput] = useState('0x00000000')
-  const [start, setStart] = useState(input)
-  const [displayedData, setDisplayedData] = useState()
 
   /** Add hex and dec. Output is hex */
   function add(a: string, b: number) {
@@ -95,37 +93,6 @@ export default function DMATable({ data, configs }: Props) {
             </button>
           </div>
         </div>
-      </div>
-      <div className="h-[calc(100vh-60px)] overflow-auto">
-        <h2 className="font-bold">DMA Buffer</h2>
-        <TableContainer component={Paper}>
-          <Table sx={styles.table} stickyHeader>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center" className="max-w-1 font-bold">
-                  Memory Address
-                </TableCell>
-                {/* <TableCell align="center">Dec</TableCell> */}
-                <TableCell align="center" className="max-w-2 font-bold">
-                  Hex
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data &&
-                data.map((value) => (
-                  <TableRow
-                    key={value.name}
-                    sx={value.value !== '0x00000000' ? { backgroundColor: '#fff177' } : {}}
-                  >
-                    <TableCell className="max-w-1">{value.name}</TableCell>
-                    {/* <TableCell>{parseInt(value.value, 16)}</TableCell> */}
-                    <TableCell className="max-w-2">{value.value}</TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
       </div>
     </div>
   )
